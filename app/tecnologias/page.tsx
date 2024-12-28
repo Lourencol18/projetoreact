224e1668c50a8dc698dff8ae35fc73ffeaf1f15b
Lourenço-a22406954 <161496221+Lourencol18@users.noreply.github.com>
2024-12-28 21:13:52 +0000

    lab 12 1.0

--- app/tecnologias/page.tsx
+++ app/tecnologias/page.tsx
@@ -1,38 +1,7 @@
-'use client';
-import tecnologias from '@/app/data/tecnologias.json';
-import Card from '@/app/components/Card/Card';
-import { Produto } from '../models/interfaces';
+import React from 'react'
 
-export default function TecnologiasPage() {
-  // Função vazia apenas para satisfazer a interface do Card
-  const addToCart = (produto: Produto) => {
-    // Não faz nada pois não precisamos desta funcionalidade na página de tecnologias
-  }
-
+export default function page() {
   return (
-    <div>
-      <h1>Tecnologias</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
-        {tecnologias.map((tecnologia) => {
-          // Convertendo tecnologia para o formato Produto
-          const produto: Produto = {
-            id: tecnologia.title, // usando title como id já que é único
-            title: tecnologia.title,
-            image: tecnologia.image,
-            description: tecnologia.description,
-            price: tecnologia.rating,
-            category: 'tecnologia'
-          }
-
-          return (
-            <Card
-              key={produto.id}
-              produto={produto}
-              addToCart={addToCart}
-            />
-          )
-        })}
-      </div>
-    </div>
-  );
+    <div>page</div>
+  )
 }
